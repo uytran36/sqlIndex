8af371686a9b8706c9bde69572f365af801ab9d6
Trần Khánh Gia Uy <uytran36@gmail.com>
2021-06-23 15:18:33 +0700

    fix bug

--- CuaHangSQL/src/main/java/com/example/cuahangsql/Controller/CtHoaDonController.java
+++ CuaHangSQL/src/main/java/com/example/cuahangsql/Controller/CtHoaDonController.java
@@ -10,13 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 @Controller
 public class CtHoaDonController {
     @Autowired
     private CtHoaDonService ctHoaDonService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Autowired
     private SanPhamService sanPhamService;
     @Autowired
@@ -61,6 +63,8 @@ public class CtHoaDonController {
         }
         ra.addFlashAttribute("message", "Thêm chi tiết thành công");
         ctHoaDonService.saveCtHD(ctHoaDon);
+        String updateQuery = "UPDATE HoaDon SET TongTien = 0 WHERE MaHD = '" + ctHoaDon.getMaHD() + "'";
+        jdbcTemplate.execute(updateQuery);
         return "redirect:/addcthd";
 
     }
